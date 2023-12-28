e981231196e363ba6b90b0744f483bef6dd45bce
Leone242 <leone.tg@outlook.com>
2023-12-28 15:32:05 -0300

    craft

--- Weapon.gd
+++ Weapon.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var selected = false
+var weapon_estate = 0
 
 func _ready():
 	pass
@@ -8,6 +9,8 @@ func _ready():
 func _process(delta):
 	if Input.is_action_pressed("mouse_select"):
 		FollowMouse()
+		
+	AssembleWeapon()
 		
 func FollowMouse():
 	position = get_global_mouse_position()
@@ -24,7 +27,29 @@ func _on_area_2d_area_exited(area):
 
 func _on_area_2d_area_entered(area):
 	if area.name == "Table":
-		selected = true
-		print("foi")
-	else:
 		selected = false
+		print("foi")
+
+
+
+func AssembleWeapon():
+	match weapon_estate:
+		0:
+			get_node("Icon").show()
+			
+		1:
+			get_node("Icon2").show()
+			get_node("Icon").hide()	
+		2:
+			get_node("Icon3").show()
+			get_node("Icon2").hide()
+		3:
+			get_node("Icon4").show()
+			get_node("Icon3").hide()	
+		4:
+			get_node("Icon5").show()
+			get_node("Icon4").hide()	
+
+func _on_craft_pressed():
+	if weapon_estate <=3:
+		weapon_estate +=1
