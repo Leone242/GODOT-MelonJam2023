b6b811fcde2bd25639cef400f4d7a0be7240da6b
Leone242 <leone.tg@outlook.com>
2023-12-28 16:26:11 -0300

    craft enable/disabled

--- Weapon.gd
+++ Weapon.gd
@@ -3,18 +3,28 @@ extends Node2D
 var selected = false
 var weapon_estate = 0
 
+@export var mark: Marker2D
+
+
 func _ready():
 	pass
 	
 func _process(delta):
 	if Input.is_action_pressed("mouse_select"):
 		FollowMouse()
-		
+	elif Input.is_action_just_released("mouse_select") and !selected:
+		position = mark.position
 	AssembleWeapon()
 		
 func FollowMouse():
 	position = get_global_mouse_position()
 
+
+func _on_area_2d_area_entered(area):
+	if area.name == "Table":
+		selected = false
+		$Craft.show()
+		print("foi")
 
 
 func _on_area_2d_area_exited(area):
@@ -23,12 +33,6 @@ func _on_area_2d_area_exited(area):
 		selected = true
 	else:
 		selected = false
-
-
-func _on_area_2d_area_entered(area):
-	if area.name == "Table":
-		selected = false
-		print("foi")
 
 
 
@@ -49,7 +53,9 @@ func AssembleWeapon():
 		4:
 			get_node("Icon5").show()
 			get_node("Icon4").hide()	
+			$Craft.hide()
 
 func _on_craft_pressed():
 	if weapon_estate <=3:
 		weapon_estate +=1
+
