b7cdac4d2b6286d80e50bea5881e255a48df8a17
Leone242 <leone.tg@outlook.com>
2023-12-29 14:43:27 -0300

    weapons

--- Cursor.gd
+++ Cursor.gd
@@ -6,11 +6,18 @@ var cursorF = preload("res://sprites/cursores/MãoF.png")
 var cursorT = preload("res://sprites/cursores/MarteloF.png")
 var cursorH = preload("res://sprites/cursores/MarteloSelecionado.png")
 
+
 func _ready():
 	Input.set_custom_mouse_cursor(cursorA, Input.CURSOR_ARROW, Vector2(16, 16))
 
 func _process(delta):
 	if Input.is_action_pressed("mouse_select"):
 		Input.set_custom_mouse_cursor(cursorF, Input.CURSOR_ARROW, Vector2(16, 16))
-	else:
+	elif !Input.is_action_pressed("mouse_select"):
 		Input.set_custom_mouse_cursor(cursorA, Input.CURSOR_ARROW, Vector2(16, 16))
+
+
+
+func _on_hammer_pressed():
+	Input.set_custom_mouse_cursor(cursorH, Input.CURSOR_ARROW, Vector2(16, 16))
+	
--- Weapon.gd
+++ Weapon.gd
@@ -4,13 +4,14 @@ var selected = false
 var weapon_estate = 0
 var on_table = false
 @export var mark: Marker2D
+var cursor = preload("res://sprites/cursores/MãoA.png")
 
 
 func _ready():
 	pass
 	
 func _process(delta):
-	if Input.is_action_pressed("mouse_select"):
+	if Input.is_action_pressed("mouse_select") and selected:
 		FollowMouse()
 	elif Input.is_action_just_released("mouse_select") and on_table:
 		position = mark.position
@@ -26,7 +27,6 @@ func FollowMouse():
 func _on_area_2d_area_entered(area):
 	if area.name == "Table":
 		on_table = true
-		selected = false
 		print("foi")
 	else:
 		on_table = false
@@ -34,11 +34,7 @@ func _on_area_2d_area_entered(area):
 
 func _on_area_2d_area_exited(area):
 	if area.name == "Table":
-		print("saiu")
-		selected = true
 		on_table = false
-	else:
-		selected = false
 
 
 
@@ -63,9 +59,19 @@ func AssembleWeapon():
 			get_node("Icon5").show()
 			
 			$Craft.hide()
+			Input.set_custom_mouse_cursor(cursor, 
+				Input.CURSOR_ARROW, Vector2(16, 16))
 
 
 func _on_craft_pressed():
 	if weapon_estate <=3:
 		weapon_estate +=1
 
+
+
+func _on_area_2d_mouse_entered():
+	selected = true
+
+
+func _on_area_2d_mouse_exited():
+	selected = false
