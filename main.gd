5f57234ac70cb93de58fe22c800b517f7d822c10
Leone242 <leone.tg@outlook.com>
2023-12-30 17:39:37 -0300

    finalizar arma

--- main.gd
+++ main.gd
@@ -1,7 +1,30 @@
 extends Node2D
 
+var coin = 0
+var payment = 200
+
+func _ready():
+	$Label.text = str(coin)
 
 func _process(delta):
 	if Input.is_action_pressed("escape"):
 		get_tree().change_scene_to_file("res://menu.tscn")
 	
+
+
+func UpdateCoinText():
+	$Label.text = str(coin)
+	
+func UpdateCoin():
+	coin += payment
+	UpdateCoinText()
+
+
+func _on_table_area_exited(area):
+	if area.name == "Area2D":
+		$Finish.show()
+
+
+func _on_finish_pressed():
+	$Finish.hide()
+	UpdateCoin()
